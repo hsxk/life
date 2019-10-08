167be8dc73ccc1e4ea1ac8da9c8b876c3d4e49e6
hsxk <haokexin1214@gmail.com>
2019-10-08 16:02:06 +0900

    20191008

--- life/DocumentRoot/wp-content/plugins/life/life.php
+++ life/DocumentRoot/wp-content/plugins/life/life.php
@@ -1,19 +1,22 @@
 <?php 
 /*
 Plugin Name: life
+Version: 0.1
 Author: haokexin
 Author URI: https://hkx.monster
 */
 
-/*
-     upload webp   and  show webp in media
-*/
+/*-------------------------------------------------------------------------------
+                  Upload webp and Show webp in medialist
+-------------------------------------------------------------------------------*/
+
+/*---------------------------Allow upload--------------------------------------*/
 function add_webp_upload( $array ) {
 	$array['webp'] = 'image/webp';
 		return $array; 
 }
 add_filter( 'mime_types', 'add_webp_upload', 10, 1 );
-
+/*--------------------------Show webp in medialist-----------------------------*/
 function media_show_webp($result, $path) {
 	$info = @getimagesize( $path );
 		if($info['mime'] == 'image/webp') {
@@ -23,11 +26,14 @@ function media_show_webp($result, $path) {
 }
 add_filter( 'file_is_displayable_image', 'media_show_webp', 10, 2 );
 
-/*
-     get exit
-*/
-function get_exif(){
+/*-------------------------------------------------------------------------------
+                               Get exif
+-------------------------------------------------------------------------------*/
 
+/*-----------------------------Get exif----------------------------------------*/
+function get_exif(){
+    global $wpdb;
+	$table_name = $wpdb -> prefix . "exif";
 }
 
 
